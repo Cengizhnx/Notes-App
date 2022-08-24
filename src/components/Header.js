26e7f6adb95002488b048f99be1e0feb32f3277f
unknown <cengiz69mg@gmail.com>
2022-08-24 13:31:44 +0300

    design fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='pt-10'>
-        <h1 className='text-2xl headerText'>Notes APP</h1>
+        <h1 className='text-2xl headerText'># Notes APP</h1>
     </div>
   )
 }
